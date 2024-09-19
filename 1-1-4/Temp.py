4cd6bf450ea15417c3617f03a09d0e7b409a5196
Garnet <garnet.cheng@vtsd.com>
2024-09-19 11:46:17 -0400

    3 towers

---
+++ 1-1-4/Temp.py
@@ -0,0 +1,9 @@
+num1 = int(input("PLease enter a number"))
+num2 = int(input("PLease enter a number"))
+
+if num1 > num2:
+    print ("the second number is bigger")
+elif num2 > num1:
+    print ("the first number is bigger")
+else:
+    print ("both numbers are equal")
---
+++ 1-1-4/grades.py
@@ -0,0 +1,12 @@
+grade = int(input("PLease enter the grade you got on the last test"))
+
+if grade >= 90 and grade <= 100:
+    print ("A")
+if grade >= 80 and grade <= 89:
+    print ("B")
+if grade >= 70 and grade <= 79:
+    print ("C")
+if grade >= 65 and grade <= 69:
+    print ("D")
+if grade < 65:
+    print ("try again next year")
