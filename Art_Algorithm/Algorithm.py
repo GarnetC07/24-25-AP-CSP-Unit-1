5068534bdf03192ec1ef37733e744c9595ab115b
Garnet <garnet.cheng@vtsd.com>
2024-10-21 11:46:11 -0400

    made grass and lights

--- Art_Algorithm/Algorithm.py
+++ Art_Algorithm/Algorithm.py
@@ -6,12 +6,12 @@ light = trtl.Turtle()
 light.speed("fastest")
 light.screen.bgcolor("lightblue")
 # Start of box
-light.goto(-50, 350)
+light.goto(-50,300)
 def draw_sides():
 #drew the outlines/edges of the traffic light
     light.forward(100)
     light.right(90)
-    light.forward(300)
+    light.forward(305)
     light.right(90)
 #Begin fill in object
 light.begin_fill()
@@ -20,17 +20,13 @@ draw_sides()
 #end fill on object
 light.end_fill()
 
-#Made the red light for the traffic light
-light.goto(3,300)
-light.shape("circle")
-light.color("red")
 
 light.penup()
 light.goto(-500, -400)
-light.pensize(5)
-light.color("Green")
-
+light.pensize(7)
+light.color("green")
 #Drawing the randomized blades of grass.
+
 def draw_grass():
     length = random.randint(85, 140)
 
@@ -41,10 +37,35 @@ def draw_grass():
     light.right(180)
     light.forward(length)
     light.left(90)
-    light.forward(20)
+    light.forward(10)
     light.left(90)
 for blades in range(100):
     draw_grass()
+
+#Made the yellow light for the traffic light
+light.penup()
+light.goto(40,250)
+light.color("red")
+light.pendown()
+light.begin_fill()
+light.circle(40)
+light.end_fill()
+
+light.penup()
+light.goto(40,150)
+light.color("yellow")
+light.pendown()
+light.begin_fill()
+light.circle(40)
+light.end_fill()
+
+light.penup()
+light.goto(40,50)
+light.color("green")
+light.pendown()
+light.begin_fill()
+light.circle(40)
+light.end_fill()
 
 wn = trtl.Screen()
 wn.mainloop()
