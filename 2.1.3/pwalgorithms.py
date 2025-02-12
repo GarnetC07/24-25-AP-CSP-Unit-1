ebac337ae98daa62adbeef8c0fbe1cee89e4ceb7
Garnet <garnet.cheng@vtsd.com>
2025-02-12 11:43:17 -0500

    2/12/2025

--- 2.1.3/pwalgorithms.py
+++ 2.1.3/pwalgorithms.py
@@ -20,3 +20,40 @@ def one_word(password):
     if (w == password):
       return True, guesses
   return False, guesses
+
+def two_word(password):
+  words = get_dictionary()
+  guesses = 0
+  # get each word from the dictionary file
+  for w in words:
+    for h in words:
+      guesses += 1
+      if (w == password):
+       return True, guesses
+  return False, guesses
+
+def two_words_and_digit(password):
+  digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  words = get_dictionary()
+  guesses = 0
+  # get each word from the dictionary file
+  for w in words:
+    for h in words:
+      for d in digits:
+        guesses += 1
+        if w + h + str(d) == password:
+          return True, guesses
+        if str(d) + w + h == password:
+         return True, guesses
+  return False, guesses
+
+def two_words_and_digit(password):
+  words = get_dictionary()
+  guesses = 0
+  # get each word from the dictionary file
+  for w in words:
+    for h in words:
+      guesses += 1
+      if (w == password):
+       return True, guesses
+  return False, guesses-
